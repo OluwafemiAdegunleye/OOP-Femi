07a68e2f2ff4e82ab87fcc92f002ca3fc7053f38
Cyril Uwagwu <cyril@lemeritegroupe.com>
2023-01-16 22:30:07 +0100

    updated

---
+++ Account.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Account
+    {
+        public string AccountName;
+        public string AccountNumber;
+        public string AccountType;
+        public string OpeningBalance;
+        public string currentBalance;
+        public string routingNumber;
+        public string BankName;
+        public string BankCode;
+        public string CustomerCode;
+        public DateTime DateCreated;
+        public string CreatedBy;
+        public DateTime DateModified;
+        public string ModifiedBy;
+        public bool AccountStatus;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP;
+
+Console.Clear();
+Customer customer = new Customer();
+Account account = new Account();
+
+Customer oauStudent = new Customer();
+Customer uiStudent = new Customer();
+
+Console.WriteLine();
+Console.WriteLine("The first customer is from " + oauStudent.Country);
+Console.WriteLine("The second customer is from " + uiStudent.Country);
+
+
+
+
+
+
+
+
+
+
+
